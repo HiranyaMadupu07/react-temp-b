e69ede3e40a51fad28197f7126bf0f05fa7681f0
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-03-26 15:22:02 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import App9 from "./components/App9";
 import App10 from "./components/App10";
 import App11 from "./components/App11";
 import App12 from "./components/App12";
+import App13 from "./components/App13";
 
 
 
@@ -32,6 +33,7 @@ function App() {
           <Link to="app10" className="box">App10</Link>
           <Link to="app11" className="box">App11</Link>
           <Link to="app12" className="box">App12</Link>
+          <Link to="app13" className="box">App13</Link>
 
         </div>
         <hr></hr>
@@ -50,6 +52,7 @@ function App() {
             <Route path="app10" element={<App10 />} />
             <Route path="app11" element={<App11 />} />
             <Route path="app12" element={<App12 />} />
+            <Route path="app13" element={<App13 />} />
 
 
           </Routes>
---
+++ src/components/App13.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+export default function App13() {
+  const [page, setPage] = useState();
+  const Navigate = useNavigate();
+  const showApp = () => {
+   Navigate(page);
+  };
+  return (
+    <div>
+      <h3>This is App13</h3>
+      <select style={{padding:'13px',margin:'10px'}}onChange={(e)=>setPage(e.target.value)}>
+      <option value="" >--Select--</option>
+        <option value="/app1">App1</option>
+        <option value="/app2">App2</option>
+        <option value="/app3">App3</option>
+        <option value="/app1">App4</option>
+        <option value="/app2">App5</option>
+        <option value="/app3">App6</option>
+        <option value="/app1">App7</option>
+        <option value="/app2">App8</option>
+        <option value="/app3">App9</option>
+        <option value="/app1">App10</option>
+        <option value="/app1">App11</option>
+        <option value="/app2">App12</option>
+        <option value="/app3">App13</option>
+      </select>
+      <button onClick={showApp}>ShowAppViaSelect</button>
+      <br></br>
+      <Link to="/app10">ShowApp10ViaLink</Link>
+    </div>
+  );
+}
