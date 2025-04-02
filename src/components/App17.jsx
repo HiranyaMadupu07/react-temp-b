75a3f850695e496c8af41bda0ed4c94e00ab675a
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-04-02 15:26:22 +0530

    new

--- src/components/App17.jsx
+++ src/components/App17.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Child from "./Child";
-
+import {useState} from "react";
 export default function App17(props) {
+    const [count,setCount]=useState(0);
+    const num=new Date()
   return (
     <div>
         <h3>App17</h3>
-        <Child num={5}/>
+        <Child num={num.getMinutes()}/>
+        <p>
+            <button onClick={()=>setCount(count+1)}>Count</button>
+        </p>
+        <p>{count}</p>
     </div>
   )
 }
