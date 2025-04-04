0371e1926cf4931f7f8b53f5cb90b260c2f675a2
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-04-04 14:51:07 +0530

    new

--- src/components/App21.jsx
+++ src/components/App21.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {useEffect,useState} from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
 export default function App21() {
-  const [count,setCount]=useState(0);
-  useEffect(()=>{
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
     console.log("Component Mounted");
-    return()=>{
+    fetch(url).then((res) => res.json().then((data) => setData(data)));
+    return () => {
       console.log("Component Unmounted");
     };
-  },[]);
+  }, []);
 
-  const handleUpdate=()=>{
-    setCount(count+1);
+  const handleUpdate = () => {
+    setCount(count + 1);
     console.log("Component Updated / Reloaded");
-  }
+  };
   return (
     <div>
       <h3>App21</h3>
@@ -20,6 +23,10 @@ export default function App21() {
       <p>It has 3 phases:i,Mounting ii,Updating iii,Unmounting</p>
       <button onClick={handleUpdate}>Update</button>
       <p>{count}</p>
+      <hr></hr>
+      {data.map((value,index)=>(
+        <div key={index}>{value.name}-{value.email}</div>
+      ))}
     </div>
-  )
+  );
 }
---
+++ src/components/App22.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <h3>App22</h3>
+    </div>
+  )
+}
